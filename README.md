bdd30c945ed664c238a72cad3a0c7acce3a3a9e2
Tyler Briles <ty8bri@gmail.com>
2014-09-12 11:42:58 -0700

    feat(bands): del band and POST new band

--- README.md
+++ README.md
@@ -1,26 +1,29 @@
 bandLeftDetailsRight
 ====================
 
-![](http://g.recordit.co/iWs9LZgi6d.gif)
+![](http://g.recordit.co/4JkJXIndzA.gif)
 
-### Requirements
-[Node.js](https://github.com/joyent/node/wiki/Installing-Node.js-via-package-manager) v0.10.30 or newer
-[redis](http://redis.io/download) `brew install redis`
+#### Requirements
+- [Node.js](https://github.com/joyent/node/wiki/Installing-Node.js-via-package-manager) v0.10.30 or newer
+- [redis](http://redis.io/download) `brew install redis`
 
-### To run locally
+#### To run locally
 1. `git clone git@github.com:MrBri/bandLeftDetailsRight.git`
 2. `npm install`
 3. `gulp` or `npm start`
 4. navigate to [http://localhost:5000](http://localhost:5000)
 
-### Approach
+#### Approach
 First got things working by using bootstrap's javascript tabs but that was basically a single page app then refactored ejs to partials and hapi's layout option.
 
 Next using HTML5's contenteditable to allow inline editing on band details POSTing to server
 
-Trying out redis's key/value and data structures
+Redis's key/value and data structures
 
-### To Do
-- POST new band
+#### To Do
 - deploy
 - user auth
+
+#### Missing
+- validation
+- add/del members
--- server.js
+++ server.js
@@ -28,7 +28,7 @@ var Hapi = require('hapi'),
     });
   },
 
-  links = function handler(request, reply) {
+  links = function links(request, reply) {
     var linksViewData = {},
       response = reply.view('index', linksViewData).hold();
 
@@ -39,13 +39,13 @@ var Hapi = require('hapi'),
     });
   },
 
-  bandInfo = function bandInfo(request, reply, id) {
+  bandInfo = function bandInfo(reply, id) {
     var arrId = id - 1,
       viewDetails = {};
       viewDetails.bandId = bandId = id;
       viewDetails.bandNames = bandNames;
 
-    // use multi
+    // Could use multi
     client.hget('band:' + arrId, 'year_formed', function(err, results) { // get year formed
       if (err) { server.log(['hget', 'error'], err); }
       viewDetails.bandYear = results;
@@ -68,7 +68,7 @@ var Hapi = require('hapi'),
 
   bandDetails = function bandHandler(request, reply) {
     var id = request.params.id;
-    bandInfo(request, reply, id);
+    bandInfo(reply, id);
   },
 
   updateBand = function updateBand(request, reply) {
@@ -76,22 +76,40 @@ var Hapi = require('hapi'),
       arrId = id - 1,
       content = request.payload.content.trim();
 
-    if (request.payload.memberId) { // either inputing a member, band name or year
+    if (request.payload.memberId) { // either input a member, input band name, delete band or input year
       client.hset('members:' + arrId, request.payload.memberId, content, redis.print);
     } else if (request.payload.isBandName) {
-
       client.hset('band:' + arrId, 'name', content, redis.print);
       client.hset('bands', arrId, content, redis.print);
+      bandNames = []; // grab band names on next display
+    } else if (request.payload.toDelete) {
+      client.del('band:' + arrId, redis.print);
+      client.del('members:' + arrId, redis.print);
+      client.hdel('bands', arrId, redis.print);
       bandNames = []; // grab band names on next display
     } else {
       client.hset('band:' + arrId, 'year_formed', content, redis.print);
     }
-    bandInfo(request, reply, id);
+    bandInfo(reply, id);
   },
 
-  membersSortedSet = function membersSortedSet(i, members) {
+  createBand = function createBand(request, reply) {
+    var arrId = bandNames.length,
+      band = request.payload;
+      members = band.members.split('\n');
+
+    client.hmset('band:' + arrId, 'name', band.name, 'year_formed', band.year_formed, redis.print);
+    client.hset('bands', arrId, band.name, redis.print);
+    bandNames = []; // grab band names on next display
+    membersHash(arrId, members);
+    bandInfo(reply, arrId + 1); // redirect maybe better?
+  },
+
+  membersHash = function membersHash(i, members, nx) {
+    nx = nx ? nx : ''; // whether to override or not
+
     members.forEach(function(member, j) {
-      client.hsetnx('members:' + i, j, member.name, redis.print);
+      client['hset' + nx]('members:' + i, j, member.name || member, redis.print);
     });
   };
 /*   End of variable declaration   */
@@ -101,7 +119,7 @@ client.on('error', function(err) {
 });
 
 bands.forEach(function(band, i) {
-  server.log(['json'], 'Loading default json if it doesn\'t exist into database...');
+  server.log(['json', 'redis'], 'Loading default json (if it doesn\'t exist) into database...');
   client.hsetnx('bands', i, band.name, redis.print); // Could create multi/exec, apparently pipeline is automatic in node.js
   client.exists('band:' + i, function(err, results) {
     if (err) { server.log(['hget', 'error'], err); }
@@ -109,14 +127,15 @@ bands.forEach(function(band, i) {
       client.hmset('band:' + i, 'name', band.name, 'year_formed', band.year_formed, redis.print);
     }
   });
-  membersSortedSet(i, band.members);
+  membersHash(i, band.members, 'nx');
 });
 
 /* ************* hapi ************* */
 server.route([
   { path: '/', method: 'GET', handler: links },
   { path: '/bands/{id}', method: 'GET', handler: bandDetails },
-  { path: '/bands/{id}/edit', method: 'POST', handler: updateBand }
+  { path: '/bands/{id}/edit', method: 'POST', handler: updateBand },
+  { path: '/create', method: 'POST', handler: createBand }
 ]);
 
 server.on('log', function(event, tags) {
